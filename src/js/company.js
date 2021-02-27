6fbf9115742051d5041847d4c62aad4ad18aec25
Nikita <no.colour.you.like@gmail.com>
2021-02-27 22:32:34 +0300

    company video

--- src/js/company.js
+++ src/js/company.js
@@ -7,10 +7,21 @@ hamburger.addEventListener('click', () => {
   headerNav.classList.toggle('nav-anim');
   hamburger.classList.toggle('hamburger-anim');
 
-
-
   companyBlock.forEach(item => {
     item.classList.toggle('change-width')
   });
   
 });
+
+// Video play on hover
+
+const devsBlock = document.querySelector('.company__devs'),
+  devsVideo = document.querySelector('.company__devs-video');
+
+devsBlock.addEventListener('mouseover', () => {
+  devsVideo.play();
+}); 
+
+devsBlock.addEventListener('mouseout', () => {
+  devsVideo.pause();
+}); 
