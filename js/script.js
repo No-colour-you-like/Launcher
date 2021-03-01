93920f50bc033e722226b0730b52118b36370bc4
Nikita <no.colour.you.like@gmail.com>
2021-03-01 20:40:50 +0300

    rocket process

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+//Body delay opacity
+
+setTimeout(function() {
+  
+  document.querySelector('.body').classList.add('body-visible');
+
+  setTimeout(function() {
+    document.querySelector('.body').classList.remove('body-hide');
+    document.querySelector('.body').classList.remove('body-visible')
+  }, 2000);
+
+}, 200);
+
+
+
 // Hamburger menu 
 
 const hamburger = document.querySelector('.hamburger'), 
